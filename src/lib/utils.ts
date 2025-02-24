62fe9cfdbc8c1d831e50d580f71f0824774355c5
Walter Fritsche <wfritsche95@protonmail.com>
2025-02-24 11:01:19 -0600

    added time formatting util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { formatDistanceToNowStrict } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function shortTimeAgo(date: Date): string {
+  const result = formatDistanceToNowStrict(date);
+  return result
+    .replace(" minutes", "m")
+    .replace(" minute", "m")
+    .replace(" hours", "h")
+    .replace(" hour", "h")
+    .replace(" days", "d")
+    .replace(" day", "d")
+    .replace(" weeks", "w")
+    .replace(" week", "w")
+    .replace(" months", "mo")
+    .replace(" month", "mo")
+    .replace(" years", "y")
+    .replace(" year", "y");
 }
