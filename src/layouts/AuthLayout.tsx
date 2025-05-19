b5745fcb6b5a6937adbe844a4fab20a2acbd234e
Walter Fritsche <wfritsche95@protonmail.com>
2025-05-19 09:55:12 -0500

    added a auth check component

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,5 +1,18 @@
-function AuthLayout() {
-  return <div className=""></div>;
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+function AuthLayout({ children }: AuthLayoutProps) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      navigate("/signin");
+    }
+  }, []);
+  return <>{children}</>;
 }
 
 export default AuthLayout;
